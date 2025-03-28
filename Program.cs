57984f99285c23140081a91aad272b1e7d95c790
Raimundos Marques <devraimundos@outlook.com>
2025-03-28 20:54:25 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// Supostamente isso devia mostrar os valores sem o truncamento
+
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine(d);
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine(e);
+Console.WriteLine($"remainder: {e}");
